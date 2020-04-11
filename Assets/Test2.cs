6716dc94e6264bc25259d7afa31892a8803cd8c0
yuseiikenono <i.yusei@wingjapanmembers.net>
2020-04-11 17:37:00 +0900

    Script Testのプロジェクト　Lesson4の課題再提出
    Lesson4課題の再提出です。配列を宣言して出力しました。

---
+++ Assets/Test2.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test2 : MonoBehaviour {
+
+	// Use this for initialization
+	void Start()
+	{
+		int[] array = new int[5];
+
+		array[0] = 30;
+		array[1] = 120;
+		array[2] = 180;
+		array[3] = 60;
+		array[4] = 240;
+
+		for (int i = 0; i < 5; i++)
+		{
+			for (int j = i + 1; j < 5; j++)
+			{
+				if (array[i] < array[j])
+				{
+					int a = array[i];
+					array[i] = array[j];
+					array[j] = a;
+
+				}
+			}
+
+			Debug.Log(array[i]);
+		}
+
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+
+	}
+}
